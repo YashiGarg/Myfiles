6f0317b2078934a1d8d4eb547bf5cec6dba75b07
YashiGarg <47406146+YashiGarg@users.noreply.github.com>
2019-03-01 09:29:34 +0530

    Update WalletDao.java

--- Wallet/src/com/cg/wallet/dao/WalletDao.java
+++ Wallet/src/com/cg/wallet/dao/WalletDao.java
@@ -10,7 +10,10 @@ import com.cg.wallet.bean.Customer;
 public class WalletDao implements IWalletDao {
 	Map<String, Customer> map = new HashMap<String, Customer>();
 
-	@SuppressWarnings("unlikely-arg-type")
+	/* (non-Javadoc)
+	 * @see com.cg.wallet.dao.IWalletDao#save(com.cg.wallet.bean.Customer)
+	 */
+	
 	@Override
 	public boolean save(Customer c) {
 		if (map.containsValue(c.getCusPhone())) {
@@ -22,13 +25,16 @@ public class WalletDao implements IWalletDao {
 
 	}
 
+	/* (non-Javadoc)
+	 * @see com.cg.wallet.dao.IWalletDao#findByPhone(java.lang.String)
+	 */
 	@Override
 	public Customer findByPhone(String Phone) {
-		Map<String, Customer> map2 = new HashMap<String, Customer>();
+		//Map<String, Customer> map2 = new HashMap<String, Customer>();
 		for (Map.Entry<String, Customer> map1 : map.entrySet()) {
 			if (map1.getValue().getCusPhone().equals(Phone))
-				map2.put(map1.getKey(), map1.getValue());
-			return (Customer) map2;
+				//map2.put(map1.getKey(), map1.getValue());
+			return map1.getValue();
 		}
 		return null;
 
